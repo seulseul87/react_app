c73ecffa8c81d29f34422ed8100c76d2b29d2e65
SEULGI <gcole877@gmail.com>
2022-07-17 01:57:34 -0400

    remove space

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function App() {
     return (
         <header>
-            <nav>
+            <nav>                           
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li> 
